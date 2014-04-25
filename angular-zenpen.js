7b02db731e399d20a9a5aa518fd69a519ad5dcc5
Oleg Meleshko <qu1ze34@gmail.com>
2014-04-26 02:06:38 +0300

    fixed adding and removing styles

--- angular-zenpen.js
+++ angular-zenpen.js
@@ -4,48 +4,66 @@
 	'use strict';
 
 	angular.module('angularZenpen', ['angular.css.injector'])
-		.directive('zEditor', ['$timeout', '$window', 'cssInjector', function ($timeout, $window, cssInjector) {
+    .config(function(cssInjectorProvider){
+        cssInjectorProvider.setSinglePageMode(true);
+    })
+		.directive('zEditor', ['$rootScope', '$timeout', '$window', 'cssInjector', function ($rootScope, $timeout, $window, cssInjector) {
       
-      var template = '<div class="section yin">' + 
-                        '<div class="ui">' +
-                          '<div class="wrapper">' +
-                            '<div class="top editing">' + 
-                              '<button class="color-flip useicons" title="Invert colors">&#xe002;</button>' +
-                            '</div>' +
+      var baseUrl = '/bower_components/'
+        , template = '<div class="section yin">' + 
+                      '<div class="ui">' +
+                        '<div class="wrapper">' +
+                          '<div class="top editing">' + 
+                            '<button class="color-flip useicons" title="Invert colors">&#xe002;</button>' +
                           '</div>' +
-                        '</div>' +	
-                        '<div class="text-options">' +
-                          '<div class="options">' +
-                            '<span class="no-overflow">' +
-                              '<span class="lengthen ui-inputs">' +
-                                '<button class="url useicons">&#xe005;</button>' +
-                                '<input class="url-input" type="text" placeholder="Type or Paste URL here"/>' +
-                                '<button class="bold">b</button>' +
-                                '<button class="italic">i</button>' +
-                                '<button class="quote">&rdquo;</button>' +
-                              '</span>' +
+                        '</div>' +
+                      '</div>' +	
+                      '<div class="text-options">' +
+                        '<div class="options">' +
+                          '<span class="no-overflow">' +
+                            '<span class="lengthen ui-inputs">' +
+                              '<button class="url useicons">&#xe005;</button>' +
+                              '<input class="url-input" type="text" placeholder="Type or Paste URL here"/>' +
+                              '<button class="bold">b</button>' +
+                              '<button class="italic">i</button>' +
+                              '<button class="quote">&rdquo;</button>' +
                             '</span>' +
-                          '</div>' +
+                          '</span>' +
                         '</div>' +
-                        '<article contenteditable="true" class="content"></article>' +
-                        '<div class="word-counter">' +
-                          '<span class="progress complete"></span>' +
-                        '</div>' +
-                      '</div>';
+                      '</div>' +
+                      '<article contenteditable="true" class="content"></article>' +
+                      '<div class="word-counter">' +
+                        '<span class="progress complete"></span>' +
+                      '</div>' +
+                    '</div>';
       
       return {
         restrict : 'ACE',
         replace : true,
         template : template,
+        scope : {
+          ngModel : '=',
+          baseUrl : '@'
+        },
         link : function(scope, element, attrs) {
-          cssInjector.add("http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic");
-          cssInjector.add("/bower_components/zenpen/css/style.css");
-          cssInjector.add("/bower_components/zenpen/css/fonts.css");
-          $timeout(function() {            
-            if ($window.zeditor) {
-              $window.zeditor();              
-            }
-          }, 0);
+          scope.baseUrl = scope.baseUrl || baseUrl;
+          
+          cssInjector.add('http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
+          cssInjector.add(scope.baseUrl + 'zenpen/css/style.css');
+          cssInjector.add(scope.baseUrl + 'zenpen/css/fonts.css');
+      
+          if ($window.zeditor) {
+            $window.zeditor({
+              onKeyUp : function(html) {                
+                scope.ngModel = html;
+                $rootScope.safeApply();                
+              }
+            });              
+          }
+          
+          scope.$on('$destroy', function(){
+            cssInjector.removeAll();
+          });
         }
       };
 		}]);
